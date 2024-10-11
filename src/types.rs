e47be5fd60daa64c7dd2905b2080ea555620d05f
syncpark <dkpark@cluml.com>
2024-10-11 17:49:37 +0900

    Modify the derive macro of EventCategory

--- src/types.rs
+++ src/types.rs
@@ -44,7 +44,7 @@ pub struct HostNetworkGroup {
 // IP address, port numbers, and protocols.
 pub type TrafficFilterRule = (IpNet, Option<Vec<u16>>, Option<Vec<u16>>);
 
-#[derive(Clone, Copy, Debug, Deserialize_repr, Eq, PartialEq, Serialize_repr)]
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[repr(u8)]
 pub enum EventCategory {
     Unknown = 0,
